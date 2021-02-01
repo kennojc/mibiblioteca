c2525e746f92c12f7a44753c2889949679595481
Kenneth Johnson C <kennojc@gmail.com>
2021-02-01 18:31:55 -0300

    Model OK, bootstrapped

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
 validates :title, presence: true
-validates :borrower, presence: true
+validates :borrower, presence: false
 validates :status, presence: true
 
 enum status: [:estante, :prestado]
